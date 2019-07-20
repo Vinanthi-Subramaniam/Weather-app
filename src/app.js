97989399aeb64b8bee6512984d4670f42ae9d976
Vinanthi <vinanthi.subramaniam@gmail.com>
2019-07-20 13:54:04 +0530

    Init commit

---
+++ src/app.js
@@ -0,0 +1,105 @@
+const  path=require("path")
+const express=require('express')
+const hbs=require('hbs')
+const geocode=require('./utils/geocode')
+const forecast=require('./utils/forecast')
+
+const app=express()
+const publicpath=path.join(__dirname,'../public')
+const temppath=path.join(__dirname,'../templates/views')
+const partialspath=path.join(__dirname,'../templates/partials')
+
+//handlebar setup
+app.set('view engine','hbs')
+app.set('views',temppath)
+
+//path for static content 
+app.use(express.static(publicpath))//for homepage as it accesses index.
+
+//set up partials
+hbs.registerPartials(partialspath)
+
+app.get('',(req,res)=>{
+    res.render('index',{
+        title:"Weather App",
+        name:"Vinan",
+        
+    })
+})
+
+app.get('/about',(req,res)=>{
+    res.render("about",{
+        name:"Vinan",
+        age:21,
+        title:"About",
+        
+    })
+})
+
+app.get('/help',(req,res)=>{
+    res.render("help",{
+        helptext:"help me!",
+        title:"help",
+        name:"Vinan"
+    })
+})
+
+app.get('/weather',(req,res)=>{ 
+    if(!req.query.address){
+        return res.send({
+            error:"Please provide location/pin!"
+        })
+    }
+    
+        geocode(req.query.address,(error,{latitude,longitude,location}={})=>{
+            if(error){
+                return res.send({error})
+            }
+            forecast(latitude,longitude,(error,data)=>{
+                if(error){
+                   return res.send({error})
+                }
+                return res.send({
+                        forecast:data,
+                        location,
+                        address:req.query.address
+                    })
+            })
+        })       
+    })
+
+// app.get('/products',(req,res)=>{
+//     if(!req.query.search){
+//         return res.send({
+//             error:'you must provide a search term'
+//         })
+//     }
+//     console.log(req.query)
+//     res.send({
+//         products:[]
+//     })
+// })
+
+//for all help 404 errors
+app.get("/help/*",(req,res)=>{
+    res.render("404",{
+        errormessage:"Help page not found",
+        name:"Vinan",
+        title:"404 Error"
+    })
+    
+})
+//for allgeneric 404 errors
+app.get('*',(req,res)=>{
+    res.render("404",{
+        errormessage:"404 error",
+        name:"Vinan",
+        title:"404 Error"
+    })
+
+
+})
+
+app.listen(3000,()=>{
+ console.log("listening to port 3000")
+})
---
+++ src/utils/forecast.js
@@ -0,0 +1,18 @@
+const request=require('request')
+const forecast=(lat,long,callback)=>{
+    const url="https://api.darksky.net/forecast/6af60ec46cbfb01947c09632d1e1dad9/"+lat+","+long //37.8267,-122.4233"
+    request({url,json:true},(error,{body})=>{
+        if(error){
+            callback('Unable to connect to weather service',undefined)
+        } else if(body.error){
+            callback('Unable to find location',undefined)
+        } else {
+            callback(undefined, body.daily.data[0].summary +" The current temperature is "+body.currently.temperature +". The probabilty of precipitation is "+body.currently.precipProbability)
+                 
+                    
+                
+        }
+    })
+    
+}
+module.exports=forecast
